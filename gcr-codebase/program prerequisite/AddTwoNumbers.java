26ade9f49013a18feddb184b8b03b504e7b4c5b6
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-16 21:00:19 +0530

    [Hariom Patel] : added a program of AddTwoNumbers

---
+++ gcr-codebase/program prerequisite/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AddTwoNumbers{
+public static void main (String []args){
+Scanner kb = new Scanner (System.in);
+int num1=kb.nextInt();
+int num2 = kb.nextInt();
+System.out.println(a+b);
+}
+}
