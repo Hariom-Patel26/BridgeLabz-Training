293ed8d804087c30d6bc33babba0f2d7b9a9af35
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-16 21:07:21 +0530

    [Hariom Patel] : added a program of AreaofaCircle

---
+++ gcr-codebase/program prerequisite/AreaofaCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AreaofaCircle{
+public static void main (String []args){
+Scanner kb = new Scanner (System.in);
+int radius =kb.nextInt();
+double Area = 3.14 *radius*radius;
+System.out.println(Area);
+}
+}
