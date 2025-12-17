0ef270a1ff099a11eeb0992e6ac1f1ae12fb8b05
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-17 21:08:17 +0530

    [Hariom patel] : added CelsiustoFahreheitConversion java program

--- gcr-codebase/program prerequisite/CelsiustoFahreheitConversion.java
+++ gcr-codebase/program prerequisite/CelsiustoFahreheitConversion.java
@@ -1,10 +1,11 @@
 import java.util.*;
 class CelsiustoFahreheitConversion{
-public static void main (String []args){
-Scanner kb = new Scanner (System.in);
-int Celsius =kb.nextInt();
-int Temperature = (Celsius * 9/5) + 32;
-System.out.println(Temperature);
-}
+    public static void main (String []args){
+    Scanner kb = new Scanner (System.in);
+    int Celsius =kb.nextInt();
 
+    int Temperature = (Celsius * 9/5) + 32;
+
+    System.out.println(Temperature);
+    }
 }
