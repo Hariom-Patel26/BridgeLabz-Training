d5634f70894f7c09b24faf74afc6f6f98869504f
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-17 21:05:20 +0530

    [Hariom patel] : added CalculateSimpleInterest java program

--- gcr-codebase/program prerequisite/CalculateSimpleInterest.java
+++ gcr-codebase/program prerequisite/CalculateSimpleInterest.java
@@ -1,10 +1,13 @@
+import java.util.*;
 class CalculateSimpleInterest {
-public static void main (String [] args){
-Scanner kb = new Scanner (System.in);
-int Principle = kb.nextInt();
-double rate = kb.nextInt();
-double time = kb.nextInt();
-double SimpleInterest = (Principal * Rate * Time) / 100;
-System.out.println(SimpleInterest);
-}
+    public static void main (String [] args){
+    Scanner kb = new Scanner (System.in);
+    int principle = kb.nextInt();
+    double rate = kb.nextInt();
+    double time = kb.nextInt();
+
+    double SimpleInterest = ((principle * rate * time) / 100);
+
+    System.out.println(SimpleInterest);
+   }
 }
