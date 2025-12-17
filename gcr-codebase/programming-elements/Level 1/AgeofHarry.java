72fb36d0c7039bececce860798110a40326c65b6
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-17 14:55:25 +0530

    [Hariom patel] : added a java program AgeofHarry

---
+++ gcr-codebase/programming-elements/Level 1/AgeofHarry.java
@@ -0,0 +1,13 @@
+// Creating Class with name HarrysAge indicating the purpose is to Calculate the current age of Harry and //Display results.
+class AgeofHarry {
+    public static void main (String [] args){
+    // Create a int variable birhtYear and assign value 2000.
+    int birthYear = 2000;
+    // Create a int variable currentYear and assign value 2024.
+    int currentYear = 2024;
+    // Create a int variable calculateAge and calculating the age from year 2000 to year 2024.
+    int calculateAge = 2024-2000;
+    // display the age of harry in the year 2024.
+    System.out.println("Harry's age in 2024 is : "+calculateAge); 
+    }
+}
