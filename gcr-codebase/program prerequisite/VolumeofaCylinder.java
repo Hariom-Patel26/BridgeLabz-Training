cf4324fb27fe3b858ee06845396829f604264066
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-17 21:11:59 +0530

    [Hariom patel] : added VolumeofaCylinder java program

--- gcr-codebase/program prerequisite/VolumeofaCylinder.java
+++ gcr-codebase/program prerequisite/VolumeofaCylinder.java
@@ -1,10 +1,12 @@
 import java.util.*;
 class VolumeofaCylinder{
-public static void main (String []args){
-Scanner kb = new Scanner (System.in);
-int Radius =kb.nextInt();
-int  Height = kb.nextInt();
-double Volume = 3.14 * Radius *Radius *Height;
-System.out.println(Volume);
-}
+    public static void main (String []args){
+    Scanner kb = new Scanner (System.in);
+    int Radius =kb.nextInt();
+    int  Height = kb.nextInt();
+
+    double Volume = 3.14 * Radius *Radius *Height;
+
+    System.out.println(Volume);
+   }
 }
