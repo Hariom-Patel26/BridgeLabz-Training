e3791df85f4188bb5cbabdcb49a3b5b2b721168f
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-22 02:47:18 +0530

    [Hariom patel] : added a program of QuotientAndRemainder

---
+++ gcr-codebase/methods/Level 1/QuotientAndRemainder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class QuotientAndRemainder {
+
+    // Method to find quotient and remainder
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;
+        int remainder = number % divisor;
+
+        return new int[]{quotient, remainder};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number and divisor : ");
+        int number = sc.nextInt();
+        int divisor = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(number, divisor);
+
+        System.out.println("Quotient: " + result[0] + "Remainder: " + result[1]);
+        System.out.println();
+        sc.close();
+    }
+}
