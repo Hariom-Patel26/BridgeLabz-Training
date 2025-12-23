a98e062915bef9a5cde8233d96efc8217bd9c5a8
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-24 02:58:02 +0530

    [Hariom patel] : added a new program of AnagramCheck

---
+++ gcr-codebase/Extras/strings/AnagramCheck.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class AnagramCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        if (s1.length() != s2.length()) {
+            System.out.println("Not Anagrams");
+            return;
+        }
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i)]++;
+            freq[s2.charAt(i)]--;
+        }
+
+        for (int i : freq) {
+            if (i != 0) {
+                System.out.println("Not Anagrams");
+                return;
+            }
+        }
+
+        System.out.println("Anagrams");
+    }
+}
