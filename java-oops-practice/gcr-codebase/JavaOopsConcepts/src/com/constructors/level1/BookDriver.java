77dc47c9d1e0ca23ca7b27e6c90c0fa032d6ea05
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-28 00:32:07 +0530

    [Hariom patel] : Added a new program of bookDriver of main method.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/BookDriver.java
@@ -0,0 +1,13 @@
+package com.constructors.level1;
+
+public class BookDriver {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book item = new Book();
+		item.getDetails();
+		Book item2 = new Book("The Jungle Book " , "Stephen ben" ,545);
+		item2.getDetails();
+
+	}
+
+}
