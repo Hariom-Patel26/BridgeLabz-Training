586af958e3449a49a903c2ac39fa2fedeb5411b3
Hariompatel26 <hariompatel2611@gmail.com>
2025-12-29 11:41:40 +0530

    [Hariom patel] : Added a new program of employee.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/Employee.java
@@ -0,0 +1,22 @@
+package com.constructors.level1;
+
+public class Employee {
+	    public int employeeID;
+	    protected String department;
+	    private double salary;
+
+	    Employee(int employeeID, String department, double salary) {
+	        this.employeeID = employeeID;
+	        this.department = department;
+	        this.salary = salary;
+	    }
+
+	    // Public method to modify salary
+	    public void setSalary(double salary) {
+	        this.salary = salary;
+	    }
+
+	    public double getSalary() {
+	        return salary;
+	    }
+	}
