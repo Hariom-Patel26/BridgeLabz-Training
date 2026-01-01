5c4c39fa9cca99b51c59f8a2e8070b000b57b4c1
Hariompatel26 <hariompatel2611@gmail.com>
2026-01-01 14:46:53 +0530

    [Hariom] : UniversityDemo.java program file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/levelone/Department1.java
@@ -0,0 +1,11 @@
+package com.objectmodeling.levelone;
+
+public class Department1 {
+
+    String departmentName;
+
+    public Department1(String departmentName) {
+        this.departmentName = departmentName;
+    }
+    
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/levelone/Faculty.java
@@ -0,0 +1,11 @@
+package com.objectmodeling.levelone;
+
+public class Faculty {
+
+    String name;
+
+    public Faculty(String name) {
+        this.name = name;
+    }
+    
+}
