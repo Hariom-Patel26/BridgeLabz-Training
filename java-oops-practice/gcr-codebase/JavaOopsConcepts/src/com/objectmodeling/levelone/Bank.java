2eb8f83011f4302e0ba6844a7f7596ee41c1f0be
Hariompatel26 <hariompatel2611@gmail.com>
2026-01-01 12:32:06 +0530

    [Hariom] : Added Bank.java program file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/levelone/Bank.java
@@ -0,0 +1,15 @@
+package com.objectmodeling.levelone;
+
+public class Bank {
+    String bankName;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public void openAccount(Customer customer, double initialAmount) {
+        customer.balance += initialAmount;
+        System.out.println("Account opened for " + customer.name +
+                " in " + bankName);
+    }
+}
