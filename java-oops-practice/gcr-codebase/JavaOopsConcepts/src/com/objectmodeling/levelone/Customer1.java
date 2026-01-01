aca2b919493590b199483b86218e1bc6de5ee500
Hariompatel26 <hariompatel2611@gmail.com>
2026-01-01 14:39:03 +0530

    [Hariom] : Added Ecommerce.java program file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/levelone/Customer1.java
@@ -0,0 +1,16 @@
+package com.objectmodeling.levelone;
+
+public class Customer1 {
+
+    String name;
+
+    public Customer1(String name) {
+        this.name = name;
+    }
+
+    public void placeOrder(Order order) {
+        System.out.println(name + " placed an order:");
+        order.showOrderDetails();
+    }
+    
+}
