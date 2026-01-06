7898035dcc83ca36c8b59779524476c6af67879d
Hariompatel26 <hariompatel2611@gmail.com>
2026-01-06 17:00:37 +0530

    [Hariom Patel] : Added program files of Pair With Given Sum problem

---
+++ gcr-codebase/DSAConcepts/src/com/hashmaps/CheckforaPairwithGivenSuminanArray/PairWithGivenSumMain.java
@@ -0,0 +1,11 @@
+package com.hashmaps.CheckforaPairwithGivenSuminanArray;
+
+public class PairWithGivenSumMain {
+    public static void main(String[] args) {
+
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        PairWithGivenSum.hasPair(arr, target);
+    }
+}
