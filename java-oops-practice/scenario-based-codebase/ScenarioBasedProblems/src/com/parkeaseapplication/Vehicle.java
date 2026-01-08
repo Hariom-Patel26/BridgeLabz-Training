e95f9c262734dd64d9f30a8696df56cbf58fa20f
Hariompatel26 <hariompatel2611@gmail.com>
2026-01-08 15:26:54 +0530

    [Hariom patel] : Added a program files of Park Ease Application

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/parkeaseapplication/Vehicle.java
@@ -0,0 +1,19 @@
+package com.parkeaseapplication;
+
+// Base class for vehicles
+public abstract class Vehicle implements IPayable {
+
+    protected String vehicleNumber;
+    protected String type;
+
+    // constructor
+    public Vehicle(String vehicleNumber, String type) {
+        this.vehicleNumber = vehicleNumber;
+        this.type = type;
+    }
+
+    // getter for type of vehicle
+    public String getType() {
+        return type;
+    }
+}
